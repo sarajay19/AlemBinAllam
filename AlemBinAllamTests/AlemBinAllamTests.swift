0799cd1f67f0927fd91a43ab097306dd5b795df6
Sarajay19 <sarajayg19@gmail.com>
2024-11-07 19:14:06 +0300

    Initial Commit

---
+++ AlemBinAllamTests/AlemBinAllamTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlemBinAllamTests.swift
+//  AlemBinAllamTests
+//
+//  Created by Sara AlQuwaifli on 07/11/2024.
+//
+
+import Testing
+@testable import AlemBinAllam
+
+struct AlemBinAllamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
